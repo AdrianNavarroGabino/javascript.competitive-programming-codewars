3aea16ceed56444fd3d09500470981b2122270b2
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-02-09 21:40:15 +0100

    002 - Convert boolean values to strings Yes or No

---
+++ 002-boolToWord.js
@@ -0,0 +1,20 @@
+const boolToWord = (bool) => bool && 'Yes' || 'No';
+
+/*
+Instructions:
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+/*
+Sample Tests:
+
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+  it("Testing for basic tests", () => {
+    assert.strictEqual(boolToWord(true), 'Yes')
+    assert.strictEqual(boolToWord(false), 'No')
+    });
+  });
+*/
