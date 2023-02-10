48ea7502a9200dc4bc3c116d33f5568ffe60cb42
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-02-10 23:05:57 +0100

    011 - Exes and Ohs

---
+++ 011-exesAndOhs.js
@@ -0,0 +1,31 @@
+const XO = (str) => str.toLowerCase().split('').filter(s => s === 'x').length ===
+    str.toLowerCase().split('').filter(s => s === 'o').length;
+
+/*
+Instructions:
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+/*
+Sample Tests:
+
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(XO('xo'),true);
+Test.assertEquals(XO("xxOo"),true);
+Test.assertEquals(XO("xxxm"),false);
+Test.assertEquals(XO("Oo"),false);
+Test.assertEquals(XO("ooom"),false);
+  });
+*/
