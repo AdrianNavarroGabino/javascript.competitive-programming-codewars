a7431e916a7783615cae2e507e8d981d95a3451a
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-04-09 21:53:02 +0200

    073 - Beginner Series #4 Cockroach

---
+++ 073-cockroachSpeed.js
@@ -0,0 +1,28 @@
+const cockroachSpeed = (s) => parseInt(s / 0.036);
+
+/*
+Instructions:
+
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+*/
+
+/*
+Samples:
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic Tests", function() {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(cockroachSpeed(1.08), 30);
+    assert.strictEqual(cockroachSpeed(1.09), 30);
+    assert.strictEqual(cockroachSpeed(0), 0);
+  });
+});
+*/
