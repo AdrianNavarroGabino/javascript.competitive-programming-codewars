1760f99d6140335875a3c635c8a51407d9a7b8e7
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-04-25 08:21:30 +0200

    094 - Convert a Number to a String!

---
+++ 094-numberToString.js
@@ -0,0 +1,25 @@
+const numberToString = (n) => n.toString();
+
+/*
+Instructions:
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+*/
+
+/*
+Samples:
+
+const assert = require('chai').assert;
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(numberToString(67), '67');
+  });
+});
+*/
