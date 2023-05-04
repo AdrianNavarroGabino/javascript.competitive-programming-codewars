f6507b95bd19cd351b50204ab42ffa2b4fc49c8e
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-05-04 21:24:58 +0200

    119 - Grasshopper - Messi Goals

---
+++ 119-messiGoals.js
@@ -0,0 +1,38 @@
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+/*
+Instructions:
+
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+*/
+
+/*
+Samples:
+
+const Test = require('@codewars/test-compat');
+
+describe("total goals", function(){
+    it ("should equal the total", function () {
+      Test.assertEquals(totalGoals, 58)
+    })
+})
+*/
