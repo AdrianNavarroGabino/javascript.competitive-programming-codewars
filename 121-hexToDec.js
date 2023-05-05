6b498377022afa21222cedde191f97ed91352494
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-05-05 22:16:58 +0200

    121 - Hex to Decimal

---
+++ 121-hexToDec.js
@@ -0,0 +1,25 @@
+const hexToDec = (hexString) => parseInt(hexString, 16);
+
+/*
+Instructions:
+
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+/*
+Samples:
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(hexToDec("1"), 1);
+    assert.strictEqual(hexToDec("a"), 10);
+    assert.strictEqual(hexToDec("10"), 16);
+    assert.strictEqual(hexToDec("FF"), 255);
+    assert.strictEqual(hexToDec("-C"), -12);
+  })
+})
+*/
