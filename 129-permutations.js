7e60a355ba04fad1d04fc2d3b6c0ff6c65c91441
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-05-07 12:01:35 +0200

    129 - So Many Permutations!

---
+++ 129-permutations.js
@@ -0,0 +1,32 @@
+const permutations = (string) => {
+    const inputArr = string.split('');
+    let result = [];
+  
+    const permute = (arr, m = []) => {
+      if (arr.length === 0) {
+        result.push(m)
+      } else {
+        for (let i = 0; i < arr.length; i++) {
+          let curr = arr.slice();
+          let next = curr.splice(i, 1);
+          permute(curr.slice(), m.concat(next))
+       }
+     }
+   }
+  
+   permute(inputArr)
+  
+   return [...new Set(result.map(el => el.join('')))];
+}
+
+/*
+Instructions:
+
+
+*/
+
+/*
+Samples:
+
+
+*/
