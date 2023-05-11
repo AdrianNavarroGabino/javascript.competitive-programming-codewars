2b65e17d4cc4af3828f6b71d46b147da06b031fa
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-05-11 23:18:25 +0200

    134 - Vowel Count

---
+++ 134-getCount.js
@@ -0,0 +1,23 @@
+const getCount = (str) => str.toLowerCase().split('').filter(a => 'aeiou'.includes(a)).length;
+
+/*
+Instructions:
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+/*
+Samples:
+
+const {assert} = require("chai");
+
+describe("Vowels Count Tests",function(){
+  it("should return 5 for 'abracadabra'",function(){
+    assert.strictEqual(getCount("abracadabra"), 5) ;
+  });
+});
+*/
