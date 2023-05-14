1cd3178a42caf78a35d37ed7a3070bb0e493fc0d
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-05-14 18:57:22 +0200

    137 - MakeUpperCase

---
+++ 137-makeUpperCase.js
@@ -0,0 +1,22 @@
+const makeUpperCase = (str) => str.toUpperCase();
+
+/*
+Instructions:
+
+Write a function which converts the input string to uppercase.
+*/
+
+/*
+Samples:
+
+const { assert } = require('chai');
+
+describe("Basic Tests", function(){
+  it("should pass the basic tests", function(){
+    assert.strictEqual(makeUpperCase(""),           "");
+    assert.strictEqual(makeUpperCase("hello"), "HELLO");
+    assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+    assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
+  });
+});
+*/
