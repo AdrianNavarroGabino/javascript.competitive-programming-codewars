b724a91fc0fadcbea58b3bb3ca46f6591f61e755
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-05-15 22:44:01 +0200

    139 - Most Likely

---
+++ 139-mostLikely.js
@@ -0,0 +1,26 @@
+const mostLikely = (prob1, prob2) => eval(prob1.replace(':', '/')) > eval(prob2.replace(':', '/'));
+
+/*
+Instructions:
+
+Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+
+For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+So:
+
+Input: ('1:3','1:2') - returns false as 1 in 3 is less likely than 1 in 2.
+*/
+
+/*
+Samples:
+
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(mostLikely('1:3','1:2'),false);
+
+  });
+});
+*/
