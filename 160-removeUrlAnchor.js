5eed91c62cb2a8f0111340809c2556853c7e77fb
Adrián Navarro Gabino <33558391+AdrianNavarroGabino@users.noreply.github.com>
2023-06-01 16:07:08 +0200

    Remove anchor from URL

---
+++ 160-removeUrlAnchor.js
@@ -0,0 +1,25 @@
+const removeUrlAnchor = (url) => url.split('#')[0];
+
+/*
+Instructions:
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
+
+/*
+Samples:
+
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(removeUrlAnchor('www.codewars.com#about'), 'www.codewars.com')
+    assert.strictEqual(removeUrlAnchor('www.codewars.com/katas/?page=1#about'), 'www.codewars.com/katas/?page=1')
+    assert.strictEqual(removeUrlAnchor('www.codewars.com/katas/'), 'www.codewars.com/katas/')
+    })
+  })
+*/
