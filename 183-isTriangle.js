74de39b5f2a473eb6732056026feec57832d7827
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2023-06-10 21:59:31 +0200

    183 - Is this a triangle?

---
+++ 183-isTriangle.js
@@ -0,0 +1,22 @@
+const isTriangle = (a, b, c) => Math.abs(a - b) < c && Math.abs(a - c) < b && Math.abs(c - b) < a;
+
+/*
+Instructions:
+
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+*/
+
+/*
+Samples:
+
+const { assert } = require("chai")
+
+describe("Public tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(isTriangle(1,2,2), true);
+    assert.strictEqual(isTriangle(7,2,2), false);
+  });
+})
+*/
