610810ffff68cf4092d05db0514bb2b967a233a5
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2023-06-12 08:24:52 +0200

    188 - Meeting

---
+++ 188-meeting.js
@@ -0,0 +1,53 @@
+const meeting = (s) => s.toUpperCase().split(';').sort((a, b) => {
+    a = a.split(':');
+    b = b.split(':');
+    const name = a[1] < b[1] ? -1 : 1;
+    const surname = a[0] < b[0] ? -1 : 1;
+    return a[1] === b[1] ? surname : name;
+}).map(name => `(${name.split(':')[1]}, ${name.split(':')[0]})`).join('');
+
+/*
+Instructions:
+
+John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.
+
+Notes
+You can see another examples in the "Sample tests".
+*/
+
+/*
+Samples:
+
+const Test = require('@codewars/test-compat');
+
+function testing(s, exp) {
+    console.log("Testing:\n", s)
+    let ans = meeting(s)
+    console.log("Actual:\n", ans)
+    console.log("Expect:\n", exp)
+    Test.assertEquals(ans, exp)
+}
+
+describe("Basic tests",function() {
+    it("meeting",function() {
+        testing("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn", 
+            "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)");
+        testing("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell", 
+            "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)");
+        testing("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern", 
+            "(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)");
+
+})})
+*/
