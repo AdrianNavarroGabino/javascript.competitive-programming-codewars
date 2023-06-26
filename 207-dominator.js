7beabde25f0a2fe2e58bb1042252985148cdf03b
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2023-06-26 08:18:43 +0200

    207 - What dominates your array?

---
+++ 207-dominator.js
@@ -0,0 +1,44 @@
+const dominator = (arr) => {
+    const obj = arr.reduce((acc, curr) => {
+        if (!acc[curr]) {
+            acc[curr] = 0;
+        }
+        acc[curr]++;
+        return acc;
+    }, {});
+
+    for(let key in obj) {
+        key = parseInt(key);
+        if(obj[key] > arr.length / 2) {
+            return key;
+        }
+    }
+
+    return -1;
+}
+
+/*
+Instructions:
+
+A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value that occurs in more than half of the elements of arr.
+For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns the dominator of arr. The function should return −1 if array does not have a dominator. All values in arr will be >=0.
+*/
+
+/*
+Samples:
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("What dominates your array?", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(dominator([3,4,3,2,3,1,3,3]),3);
+    assert.strictEqual(dominator([1,2,3,4,5]),-1);
+    assert.strictEqual(dominator([1,1,1,2,2,2]),-1);
+    assert.strictEqual(dominator([1,1,1,2,2,2,2]),2);
+  })  
+});
+*/
