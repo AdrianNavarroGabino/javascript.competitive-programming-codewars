800b53a70602b40272afa8b9ac1dc3abb6609284
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2023-07-01 12:04:17 +0200

    212 - Basic Mathematical Operations

---
+++ 212-basicOp.js
@@ -0,0 +1,32 @@
+const basicOp = (operation, value1, value2) => eval(`${value1}${operation}${value2}`);
+
+/*
+Instructions:
+
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+
+/*
+Samples:
+
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+console.log("Basic tests\n");
+Test.assertSimilar(basicOp('+', 4, 7), 11);
+Test.assertSimilar(basicOp('-', 15, 18), -3);
+Test.assertSimilar(basicOp('*', 5, 5), 25);
+Test.assertSimilar(basicOp('/', 49, 7), 7);
+  });
+});
+*/
