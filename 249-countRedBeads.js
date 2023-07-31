add04720c5cdd6cbf58bd68c9ea6a0b72dbb6f82
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2023-07-31 07:59:07 +0200

    249 - Simple beads count

---
+++ 249-countRedBeads.js
@@ -0,0 +1,27 @@
+const countRedBeads = (n) => n < 2 ? 0 : (n - 1) * 2;
+
+/*
+Instructions:
+
+Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.
+*/
+
+/*
+Samples:
+
+const Test = require('@codewars/test-compat');
+
+describe("Testing", function(){
+  it("Basic tests", function(){
+    Test.assertEquals(countRedBeads(0), 0);
+    Test.assertEquals(countRedBeads(1), 0);
+    Test.assertEquals(countRedBeads(3), 4);
+    Test.assertEquals(countRedBeads(5), 8);
+  });
+});
+*/
