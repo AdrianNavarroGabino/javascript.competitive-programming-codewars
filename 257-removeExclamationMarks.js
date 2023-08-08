84d0022d6b719a845507426bc95dd708468d16aa
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2023-08-08 07:40:30 +0200

    257 - Remove exclamation marks

---
+++ 257-removeExclamationMarks.js
@@ -0,0 +1,21 @@
+const removeExclamationMarks = (s) => s.replace(/!/g, '');
+
+/*
+Instructions:
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+/*
+Samples:
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(removeExclamationMarks("Hello World!"), "Hello World");
+  });
+});
+*/
