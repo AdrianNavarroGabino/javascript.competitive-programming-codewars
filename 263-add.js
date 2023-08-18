eafa1050dfb30b19ba9b9fa135e108458103ec90
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2023-08-18 07:41:26 +0200

    263 - Functional Addition

---
+++ 263-add.js
@@ -0,0 +1,25 @@
+const add = (n) => (m) => n + m;
+
+/*
+Instructions:
+
+Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+var addOne = add(1);
+addOne(3); // 4
+
+var addThree = add(3);
+addThree(3); // 6
+*/
+
+/*
+Samples:
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(add(1)(3), 4, 'add one to three equals four')
+  });
+});
+*/
