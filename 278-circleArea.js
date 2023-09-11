f4d4bbaeb1205ac7174c554211f13e36b0265c52
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-09-11 07:20:02 +0200

    278 - Area of a Circle

---
+++ 278-circleArea.js
@@ -0,0 +1,47 @@
+const circleArea = (radius) => {
+    if(radius <= 0) {
+        throw new Error();
+    }
+
+    return radius * radius * Math.PI;
+}
+
+/*
+Instructions:
+
+Complete the function which will return the area of a circle with the given radius.
+
+Returned value is expected to be accurate up to tolerance of 0.01.
+
+If the radius is not positive, throw Error.
+
+Example:
+
+circleArea(43.2673);     // returns 5881.248  (± 0.01)
+circleArea(68);          // returns 14526.724 (± 0.01)
+circleArea(0);           // throws Error
+circleArea(-1);          // throws Error
+*/
+
+/*
+Samples:
+
+const chai = require("chai");
+const assert = chai.assert;
+
+describe("Sample tests", function() {
+  it("Test positive", function() {
+    assert.closeTo(circleArea(1), 3.141, 0.01)
+  });
+  it("Test examples", function() {
+    assert.closeTo(circleArea(43.2673),  5881.248, 0.01)
+    assert.closeTo(circleArea(68     ), 14526.724, 0.01)
+  });
+  it("Test zero", function() {
+    assert.throws(() => circleArea(0), Error)
+  });
+  it("Test negative", function() {
+    assert.throws(() => circleArea(-1), Error)
+  });
+});
+*/
